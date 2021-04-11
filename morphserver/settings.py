36b8cdff78db835cc64309a4c471ad14dd0f15f8
SomeChars <43200522+SomeChars@users.noreply.github.com>
2021-04-11 16:54:49 +0300

    Update settings.py

--- morphserver/settings.py
+++ morphserver/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$g4gtv(7(33$8p8sy1=v8kmv71f7^)yd^$)d*s(3zg-q@v20xo'
+SECRET_KEY = 'is a secret, you retard'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
