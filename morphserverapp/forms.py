b0272bd79441596c293a8aceb3b86d8c48ac203a
SomeChars <stepan@DESKTOP-0MRP49M.localdomain>
2021-04-11 21:13:21 +0300

    I decided to keep manally made forms

--- morphserverapp/forms.py
+++ morphserverapp/forms.py
@@ -1,15 +1,17 @@
-from django import forms
-from .models import User
+# from django import forms
+# from .models import User
+#
+# class RegistrationForm(forms.Form):
+#     name = forms.CharField(max_length=32,label='Name\n')
+#     email = forms.EmailField(label='Email\n')
+#     password = forms.CharField(label='Password\n', widget=forms.PasswordInput)
+#     password_validation = forms.CharField(label='Password validation\n', widget=forms.PasswordInput)
+#
+#
+#
+# class SignInForm(forms.Form):
+#     email = forms.EmailField(label='Email')
+#     password = forms.CharField(label='Password', widget=forms.PasswordInput)
+#     remember_me = forms.BooleanField(widget=forms.CheckboxInput,label='Remember me',initial=False)
 
-class RegistrationForm(forms.Form):
-    name = forms.CharField(max_length=32,label='Name\n')
-    email = forms.EmailField(label='Email\n')
-    password = forms.CharField(label='Password\n', widget=forms.PasswordInput)
-    password_validation = forms.CharField(label='Password validation\n', widget=forms.PasswordInput)
-
-
-
-class SignInForm(forms.Form):
-    email = forms.EmailField(label='Email\n')
-    password = forms.CharField(label='Password\n', widget=forms.PasswordInput)
-    remember_me = forms.BooleanField(widget=forms.CheckboxInput,label='Remember me',initial=False)
+#forms are cool, but they were looking bad
--- morphserverapp/views.py
+++ morphserverapp/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import MorphRequest,User
-from .forms import RegistrationForm,SignInForm
 from django.contrib.auth import hashers
 
 # Create your views here.
@@ -29,53 +28,50 @@ def help(request):
 
 
 def sign_up(request):
+
     sign_up_template = loader.get_template('morphserverapp/sign_up.html')
+
     if request.method == 'POST':
-        registration_form = RegistrationForm(request.POST)
-        if registration_form.is_valid():
 
-            new_user = User.create_user({'name':request.POST.get('name'),'email':request.POST.get('email'),
-                                         'password':hashers.make_password(request.POST.get('password'))})
+        new_user = User.create_user({'name': request.POST.get('name'), 'email': request.POST.get('email'),
+                                     'password': hashers.make_password(request.POST.get('password'))})
 
-            if request.POST.get('password') != request.POST.get('password_validation'):
-                registration_form = RegistrationForm()
-                # TODO: this could be improved with message system
-                return HttpResponseRedirect('./sign_up')
+        if request.POST.get('password') != request.POST.get('password_confirmation'):
+            # TODO: this could be improved with message system
+            return HttpResponseRedirect('./sign_up')
 
-            new_user.save()
+        new_user.save()
 
-            request.session['name'] = new_user.name
-            request.session['email'] = new_user.email
-            request.session.set_expiry(0)
-            return HttpResponseRedirect('../')
-    else:
-        registration_form = RegistrationForm()
-    return HttpResponse(sign_up_template.render({'registration_form':registration_form},request))
+        request.session['name'] = new_user.name
+        request.session['email'] = new_user.email
+        request.session.set_expiry(0)
+
+        return HttpResponseRedirect('../')
+
+    return HttpResponse(sign_up_template.render(request=request))
 
 
 def sign_in(request):
+
     sign_in_template = loader.get_template('morphserverapp/sign_in.html')
+
     if request.method == 'POST':
-        sign_in_form = SignInForm(request.POST)
-        if sign_in_form.is_valid():
+        # password check
+        input_email = request.POST.get('email')
+        found_user = User.objects.get(email=input_email)
+        if not hashers.check_password(request.POST.get('password'), found_user.password):
+            sign_in_form = SignInForm()
+            # TODO: this could be improved with message system
+            return HttpResponseRedirect('./sign_in')
 
-            #password check
-            input_email = request.POST.get('email')
-            found_user = User.objects.get(email=input_email)
-            if not hashers.check_password(request.POST.get('password'),found_user.password):
-                sign_in_form = SignInForm()
-                # TODO: this could be improved with message system
-                return HttpResponseRedirect('./sign_in')
+        request.session['name'] = found_user.name
+        request.session['email'] = found_user.email
 
-            request.session['name'] = found_user.name
-            request.session['email'] = found_user.email
-            if request.POST.get('remember_me'):
-                #TODO: NOTE! this is an example of lazyness, needs rework
-                request.session.set_expiry(2592000)
-            else:
-                request.session.set_expiry(0)
-            return HttpResponseRedirect('../')
-            # return HttpResponse(sign_in_success_template.render({'user_greeting':'Signed as'+found_user.name},request))
-    else:
-        sign_in_form = SignInForm()
-    return HttpResponse(sign_in_template.render({'sign_in_form':sign_in_form},request))
+        if request.POST.get('remember_me'):
+            # TODO: NOTE! this is an example of lazyness, needs rework
+            request.session.set_expiry(2592000)
+        else:
+            request.session.set_expiry(0)
+        return HttpResponseRedirect('../')
+
+    return HttpResponse(sign_in_template.render(request=request))
